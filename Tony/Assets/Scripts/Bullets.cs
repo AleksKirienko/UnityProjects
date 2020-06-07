79acf4e012ca19a8a60c8d0cc288f582d04aeed9
AleksKirienko <aleks.Kirienko@bk.ru>
2020-06-07 20:22:20 +0700

    Add a small 3D game with Tony Stark

---
+++ Tony/Assets/Scripts/Bullets.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour {
+    public float speed;
+
+    void Update()
+    { 
+    	transform.Translate(0, 0, -speed * Time.deltaTime);
+    }
+}
---
+++ Tony/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+
+	public Text Life, winText, Timer;
+    private float time = 0.0f;
+    private int count;
+
+    void Start() {
+    	Debug.Log("Start() = " + Time.time);
+        rb = GetComponent<Rigidbody>();
+        count = 3;
+        winText.text = "";
+        setTimer();
+        setCount();
+    }
+
+    void FixedUpdate() {
+
+        setTimer();
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter (Collider other) {
+        if(other.tag == "Ball") {
+            Destroy(other.gameObject);
+            count--;
+            setCount();
+        }   
+    }
+ 
+    void setCount() {
+        Life.text = "Life: " + count.ToString();
+        
+        if(count == 0) winText.text = "Game over!";
+
+
+    }
+
+    void setTimer() {
+        time += Time.deltaTime;
+        if(count != 0) {
+            Timer.text = time.ToString();  
+            if (time >= 10.0) winText.text = "You win!";
+        }
+
+    }
+
+}
